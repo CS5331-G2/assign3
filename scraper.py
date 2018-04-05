f62f3f1b0476cb4f136de100a5666bee6a8a8cf7
Mustaqiim Muhar <mustaqiim.muhar@u.nus.edu>
2018-04-05 17:16:52 +0800

    Added process to run spider by scripting

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy.crawler import CrawlerProcess
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
@@ -18,3 +19,10 @@ class crawler(CrawlSpider):
 		item = Links()
 		item['link'] = response.url
 		yield item
+
+process = CrawlerProcess({
+	'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+	})
+
+process.crawl(crawler)
+process.start()
