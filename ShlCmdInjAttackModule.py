8ef2aefd451b1fe7a3fcd6d26bd20f226e99eb11
Qing Hua <xefrog@live.cn>
2018-04-23 01:57:38 +0800

    WIP

--- ShlCmdInjAttackModule.py
+++ ShlCmdInjAttackModule.py
@@ -1,5 +1,10 @@
 from AttackModule import AttackModule
 
+# Dangerous inputs for PHP based web servers
+# https://www.kevinlondon.com/2015/07/26/dangerous-python-functions.html
+# https://github.com/PortSwigger/command-injection-attacker
+#
+
 class ShlCmdInjAttackModule(AttackModule):
 	
 	def __init__(self):
