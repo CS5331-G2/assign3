fde185521231a9a49a920f80b7a425cef4d49be8
Mustaqiim Muhar <mustaqiim.muhar@u.nus.edu>
2018-04-24 03:56:21 +0800

    Fixed CookieSpider!

--- CookieSpider.py
+++ CookieSpider.py
@@ -1,6 +1,7 @@
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from scrapy.http import Request, FormRequest
 
 url = []
 
@@ -14,16 +15,28 @@ class Crawler(CrawlSpider):
 		)
 
 	def request(self, url, callback):
-		request = scrapy.Request(url=url, callback=callback)
+		request = scrapy.Request(url=url, callback='callback')
 		request.cookies['cookie'] = 1
 		return request
 
 	def start_requests(self):
 		for i, url in enumerate(self.start_urls):
-			yield self.request(url, self.parse_item)
+			yield Request(url, callback=self.parse_item)
 
 	def parse_item(self, response):
-		url.append(response.url)
+		# Extract All links in the particular response page
+		links = LinkExtractor().extract_links(response)
+		for link in links:
+			is_allowed = False
+			for allowed_domain in self.allowed_domains:
+				if allowed_domain in link.url:
+					is_allowed = True
+
+			if is_allowed and link.url not in url:
+				url.append(link.url)
+				yield Request(link.url, callback=self.parse_item)
+			elif is_allowed and response.url not in url:
+				url.append(response.url)
 
 	def getItems(self):
 		return url
