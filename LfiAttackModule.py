73800a2a3d0f5da7ac6286e6794b44482e9029bd
Mustaqiim Muhar <mustaqiim.muhar@u.nus.edu>
2018-04-23 21:11:22 +0800

    Remove stale code

--- LfiAttackModule.py
+++ LfiAttackModule.py
@@ -4,13 +4,3 @@ class LfiAttackModule(AttackModule):
 	
 	def __init__(self):
 		AttackModule.__init__(self, "Local File Inclusion", "Server Side Code Injection")
-
-	def attack():
-		f=open('LfiPayload.txt','r')
-		for i in f.readlines():
-
-			ur = requests.get(url+'{}'.format(i))
-			if "root" in ur.content:
-				print 'Detected LFT'
-				time.sleep(2)
-
