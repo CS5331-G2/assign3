9982b93aa2e8f91fcfc4680f6355985c6fdc8d78
Qing Hua <xefrog@live.cn>
2018-04-24 12:24:00 +0800

    Remove unused function

--- AttackReport.py
+++ AttackReport.py
@@ -32,14 +32,6 @@ class AttackReport(object):
 	def get_host(self):
 		return self.endpoint.get_scheme_and_host_url()
 
-	def get_json_dict(self):
-		s = {}
-		if self.attackClass == "Command Injection":
-			s['endpoint'] = self.endpoint.get_path()
-			s['params'] = self.formData
-			s['method'] = self.endpoint.method
-		return s
-
 	def serialize(obj):
 		s = {}
 		s['class'] = obj.attackClass
